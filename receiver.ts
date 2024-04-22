e71a498cc17ee69faa168f23b7b850af5923a14b
GHSwallow <gabriel.swallow@softwire.com>
2024-04-22 13:18:13 +0100

    on A or B press, show radio group

--- receiver.ts
+++ receiver.ts
@@ -1,3 +1,5 @@
+const radioGroup = 0
+
 radio.onReceivedString(function (receivedString) {
   if (receivedString == ".") {
     basic.showLeds(`
@@ -31,4 +33,14 @@ radio.onReceivedString(function (receivedString) {
     basic.clearScreen();
   }
 });
-radio.setGroup(1);
+input.onButtonPressed(Button.B, function () {
+  basic.showString(radioGroup);
+  basic.pause(100);
+  basic.clearScreen();
+});
+input.onButtonPressed(Button.A, function () {
+  basic.showString(radioGroup);
+  basic.pause(100);
+  basic.clearScreen();
+});
+radio.setGroup(radioGroup);
